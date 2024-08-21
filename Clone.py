1287d41b5e325f6d620859d7406c9bdf09374352
Mr. SxR <168710908+Mr-SxR@users.noreply.github.com>
2024-08-21 20:59:40 +0600

    Update Clone.py

--- Clone.py
+++ Clone.py
@@ -1,1 +1,12 @@
+import importlib,os
 
+os.system("clear")
+print("\n ~ RUN Mr.SxR ~ \n [01] System 1\n [02] System 2\n"+"="*50)
+system = input(" Choose option : ")
+
+if system in ["1","01","A","a"]:
+    requests = importlib.import_module('MrSxR_a')
+elif system in ["2","02","B","b"]:
+    pycurl = importlib.import_module('MrSxR_h')
+else:
+    exit("Invalid choice.")
