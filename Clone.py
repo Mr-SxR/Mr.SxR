eca0e1e8fbe861ae334a3956ef91590a63e14751
Mr. SxR <168710908+Mr-SxR@users.noreply.github.com>
2024-08-22 20:37:28 +0600

    Update Clone.py

--- Clone.py
+++ Clone.py
@@ -5,8 +5,8 @@ print("\n ~ RUN Mr.SxR ~ \n [01] System 1\n [02] System 2\n"+"="*50)
 system = input(" Choose option : ")
 
 if system in ["1","01","A","a"]:
-    requests = importlib.import_module('MrSxR_a')
+    api = importlib.import_module('MrSxR_a')
 elif system in ["2","02","B","b"]:
-    pycurl = importlib.import_module('MrSxR_h')
+    normal = importlib.import_module('MrSxR_h')
 else:
     exit("Invalid choice.")
