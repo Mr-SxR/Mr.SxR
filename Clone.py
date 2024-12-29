fb1e11b0c47ed91953a036e989a89644f050340b
Mr.SxR <168710908+Mr-SxR@users.noreply.github.com>
2024-12-29 11:32:34 +0600

    Update Clone.py

--- Clone.py
+++ Clone.py
@@ -1,5 +1,10 @@
-import importlib,os
+import os
 
+os.system("clear")
+print(" CLONE TOOLS OFF.....")
+exit()
+"""
+import os,importlib
 os.system("clear")
 print("\n ~ RUN Mr.SxR ~ \n [01] System 1\n [02] System 2\n"+"="*50)
 system = input(" Choose option : ")
@@ -10,3 +15,4 @@ elif system in ["2","02","B","b"]:
     normal = importlib.import_module('MrSxR_h')
 else:
     exit("Invalid choice.")
+"""
